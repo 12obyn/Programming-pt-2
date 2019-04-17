dc9765130a3ad4617b079c1480ec4622373d4574
Robyn Moncrief <44477525+12obyn@users.noreply.github.com>
2019-04-17 15:58:00 -0500

    Update Problem
    Read the comments I left in the code and update the program

--- Problem 2.py
+++ Problem 2.py
@@ -3,11 +3,11 @@
 
 #Problem 2 this will tell you the number of day of the week you returned on.
 
-day = int(input("number 0 - 6"))
+day = int(input("number 0 - 6")) # Confusing on a user part. Might say "enter"
 
-gone = int(input("How long were you gone?"))
+gone = int(input("How long were you gone?")) 
 
-gone % 7
+gone % 7 # What is this line doint?
 
-((gone % 7) + day) 
+((gone % 7) + day) # What is this line doing?
 print(((gone % 7) + day) % 7)
